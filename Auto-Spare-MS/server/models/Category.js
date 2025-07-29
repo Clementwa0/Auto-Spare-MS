3a00a90b6d94675ded070fd4b9fe3b8c3a07edc3
clementwa0 <clementwa01@gmail.com>
2025-07-29 21:07:35 +0300

    Add Mongoose Category model with name field

---
+++ Auto-Spare-MS/server/models/Category.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:true,
+            unique:true,
+            trim:true,
+        },
+    },
+    {timestamps: true}
+)
+
+export default mongoose.model('Category', categorySchema);
