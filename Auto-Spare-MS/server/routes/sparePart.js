4d3ccd2240caa9c5fa269b82b742617169c08096
clementwa0 <clementwa01@gmail.com>
2025-07-30 22:45:49 +0300

    Add /low-stock route to SparePart API for querying low inventory items

--- Auto-Spare-MS/server/routes/sparePart.js
+++ Auto-Spare-MS/server/routes/sparePart.js
@@ -6,26 +6,17 @@ const {
   updateSparePart,
   deleteSparePart,
   bulkInsert,
+  getLowStockParts,
 } = require('../controllers/sparePartController');
 
 const router = express.Router();
 
-// GET all spare parts (with optional filtering)
 router.get('/', getAllSpareParts);
-
-// GET single spare part by ID
+router.get('/low-stock', getLowStockParts); // 👈 New
 router.get('/:id', getSparePartById);
-
-// POST create new spare part
 router.post('/', createSparePart);
-
-// PUT update spare part
 router.put('/:id', updateSparePart);
-
-// DELETE spare part
 router.delete('/:id', deleteSparePart);
-
-// routes/sparePartRoutes.js
-router.post('/bulk', bulkInsert);
+router.post('/bulk-insert', bulkInsert);
 
 module.exports = router;
