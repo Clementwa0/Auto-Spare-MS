48866b656e969578601c1b009dc7c911ba7a0660
clementwa0 <clementwa01@gmail.com>
2025-07-31 12:56:38 +0300

    Integrate auth and user routes into main Express app alongside category and spare part APIs

--- Auto-Spare-MS/server/index.js
+++ Auto-Spare-MS/server/index.js
@@ -5,9 +5,11 @@ const dotenv = require('dotenv');
 const connectDB = require('./dbConfig/db');
 const categoryRoutes = require('./routes/Categories');
 const sparePartRoutes = require('./routes/sparePart');
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/users");
 
 dotenv.config();
-connectDB(); // 👈 Connect to MongoDB
+connectDB();
 
 const app = express();
 
@@ -16,6 +18,9 @@ app.use(express.json());
 
 app.use('/api/categories', categoryRoutes);
 app.use('/api/spare-parts', sparePartRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
